d86b024734616a8f16e48b78ba867acfb03ae0da
timmyreilly <timreilly@live.com>
2016-02-11 18:08:42 -0800

    colors and lat long from server

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -21,18 +21,19 @@ def home():
 def trial():
     return render_template('trial.html')
 
-python_data = {
-    'list_a': [37.767111,-122.445811],
-    'list_b': [37.792111, -122.403611]
-}
+# python_data = {
+#     'list_a': [37.767111,-122.445811]
+#     'list_b': [37.792111, -122.403611]
+# }
  
 
 @app.route('/b')
 def bingtrial():
     data = [
         {
-            "list_a": [37.767111,-122.445811] 
-            "list_b": [37.792111, -122.403611]
+            "list_a": [37.767111,-122.445811],
+            "list_b": [37.792111, -122.403611],
+            "color": [200, 200, 0, 100]
         }
     ]
     return render_template(
