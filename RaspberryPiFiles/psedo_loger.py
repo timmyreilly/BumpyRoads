beb14bf6390d5d3783e781c0efffed3473a220ab
timmyreilly <timreilly@live.com>
2016-02-18 09:23:21 -0800

    added raspberry pi files

---
+++ RaspberryPiFiles/psedo_loger.py
@@ -0,0 +1,9 @@
+# log using randomly generated numbers and store them to Azure table storage? 
+# or local stoge. 
+
+from helper import * 
+
+if __name__ == "__main__":
+    x = get_lat_long_dict(1)
+    
+
