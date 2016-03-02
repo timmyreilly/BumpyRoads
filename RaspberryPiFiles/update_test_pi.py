a5a7f62fa7a06a79e54f75246eaaee6f9a98d743
timmyreilly <timreilly@live.com>
2016-03-02 11:00:11 -0800

    new test

---
+++ RaspberryPiFiles/update_test_pi.py
@@ -0,0 +1,10 @@
+from pi_helper import *
+
+table_service=connect_to_service()
+MAX = 20 
+
+while True:
+    entry = create_random_entry()
+    for i in range(MAX):
+        insert_or_replace_entity_from_pi_azure(table_service, i, entry, 'test', 'default')
+        print entry 
