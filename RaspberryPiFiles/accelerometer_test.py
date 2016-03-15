2d5f935ca6a9129e3a67555c881b17dff9ee58c7
timmyreilly <timreilly@live.com>
2016-03-14 17:52:59 -0700

    point seven

--- RaspberryPiFiles/accelerometer_test.py
+++ RaspberryPiFiles/accelerometer_test.py
@@ -30,7 +30,7 @@ def clamp(v, min, max):
 def onAccelerometerChanged(x, y, z, lastAccel):
     #high pass filter
     updateFreq = 400 
-    RC = 0.1
+    RC = 0.7
     dt = 1.0 / updateFreq     
     alpha = dt / (dt + RC)
        
