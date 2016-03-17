cfde21fea4b5799d450b9c8b37cd5e0a197d895b
timmyreilly <timreilly@live.com>
2016-03-17 15:24:11 -0700

    was this the problem the whole time

--- RaspberryPiFiles/explore_gps.py
+++ RaspberryPiFiles/explore_gps.py
@@ -14,7 +14,7 @@ class GpsPoller(threading.Thread):
     threading.Thread.__init__(self)
     global gpsd #bring it in scope
     gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.current_value = None
+    self.current_value = gpsd.next() 
     self.running = True #setting the thread running to true
  
   def run(self):
