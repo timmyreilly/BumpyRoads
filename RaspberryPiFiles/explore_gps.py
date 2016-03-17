7126071737b364bfb14f0b6445d9cfe0385e6b3d
timmyreilly <timreilly@live.com>
2016-03-17 14:58:13 -0700

    more data!

--- RaspberryPiFiles/explore_gps.py
+++ RaspberryPiFiles/explore_gps.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
       print
       print 'sats        ' , gpsd.satellites
  
-      time.sleep(5) #set to whatever
+      time.sleep(.5) #set to whatever
  
   except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
     print "\nKilling Thread..."
