ed121058b9e16a393d9a70cc8a8bb8837d09edc8
timmyreilly <timreilly@live.com>
2016-03-17 14:56:52 -0700

    explore

--- RaspberryPiFiles/explore_gps.py
+++ RaspberryPiFiles/explore_gps.py
@@ -1,39 +1,59 @@
-import os, time
+ 
+import os
+from gps import *
+from time import *
+import time
 import threading
-from gps import *  
-
+ 
+gpsd = None #seting the global variable
+ 
+os.system('clear') #clear the terminal (optional)
+ 
 class GpsPoller(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.session = gps() 
-        self.session.stream(WATCH_ENABLE|WATCH_NEWSTYLE)
-        self.current_value = None
-        self.exitApp = False 
-        
-    def get_current_value(self):
-        return self.current_value
-        
-    def time_to_exit():
-        exitApp = True 
-        print 'exitApp now True'
-        
-    def run(self):
-        while not self.exitApp: 
-            try:
-                while True:
-                    self.current_value = self.session.next()
-            except StopIteration:
-                pass 
-        os._exit() 
-            
+  def __init__(self):
+    threading.Thread.__init__(self)
+    global gpsd #bring it in scope
+    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    self.current_value = None
+    self.running = True #setting the thread running to true
+ 
+  def run(self):
+    global gpsd
+    while gpsp.running:
+      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+ 
 if __name__ == '__main__':
-    gpsp = GpsPoller()
-    gpsp.start()
-    
-    while 1:
-        try:
-            time.sleep(.5)
-            print gpsp.get_current_value()  
-        except KeyboardInterrupt:
-            gpsdp.time_to_exit()
+  gpsp = GpsPoller() # create the thread
+  try:
+    gpsp.start() # start it up
+    while True:
+      #It may take a second or two to get good data
+      #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
+ 
+      os.system('clear')
+ 
+      print
+      print ' GPS reading'
+      print '----------------------------------------'
+      print 'latitude    ' , gpsd.fix.latitude
+      print 'longitude   ' , gpsd.fix.longitude
+      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+      print 'altitude (m)' , gpsd.fix.altitude
+      print 'eps         ' , gpsd.fix.eps
+      print 'epx         ' , gpsd.fix.epx
+      print 'epv         ' , gpsd.fix.epv
+      print 'ept         ' , gpsd.fix.ept
+      print 'speed (m/s) ' , gpsd.fix.speed
+      print 'climb       ' , gpsd.fix.climb
+      print 'track       ' , gpsd.fix.track
+      print 'mode        ' , gpsd.fix.mode
+      print
+      print 'sats        ' , gpsd.satellites
+ 
+      time.sleep(5) #set to whatever
+ 
+  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print "\nKilling Thread..."
+    gpsp.running = False
+    gpsp.join() # wait for the thread to finish what it's doing
+  print "Done.\nExiting."
