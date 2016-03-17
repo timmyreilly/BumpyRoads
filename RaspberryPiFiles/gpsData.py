6fa23aec7a1267820c25036b4248691fc1172fcf
timmyreilly <timreilly@live.com>
2016-03-17 14:02:16 -0700

    gpsData

---
+++ RaspberryPiFiles/gpsData.py
@@ -0,0 +1,43 @@
+import os, time, threading 
+from gps import * 
+
+
+gpsd = None 
+
+os.system('clear')
+
+class GpsPoller(threading.Thread):
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+        global gpsd 
+        gpsd = gps(mode=WATCH_ENABLE)
+        self.current_value = None 
+        self.running = True 
+        
+    def run(self):
+        global gpsd 
+        while gpsd.running:
+            gpsd.next() 
+            
+if __name__ == '__main__':
+    gpsp = GpsPoller() 
+    try: 
+        gpsp.start()
+        while True:
+        
+        os.system('clear')
+        
+        print 
+        print 'GPS reading'
+        print '-----------------------'
+        print 'latitude      ', gpsd.fix.latitude
+        print 'longitude     ', gpsd.fix.longitude
+        
+        
+    except (KeyboardInterrupt, SystemExit):
+        print "\nKilling Thread..."
+        gpsp.running = False 
+        gpsp.join() 
+        
+    print "Done.\nExiting" 
