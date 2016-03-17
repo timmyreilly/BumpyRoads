ca61051ed663604f5354ea0a0d25454aa0d2d550
timmyreilly <timreilly@live.com>
2016-03-17 15:45:37 -0700

    our own implementation

--- RaspberryPiFiles/gpsData.py
+++ RaspberryPiFiles/gpsData.py
@@ -1,38 +1,18 @@
-from explore_gps import *  
+from gps_poll import *
 
-            
 if __name__ == '__main__':
-  gpsp = GpsPoller() # create the thread
-  try:
-    gpsp.start() # start it up
-    while True:
-      #It may take a second or two to get good data
-      #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
- 
-      os.system('clear')
- 
-      print
-      print ' GPS reading'
-      print '----------------------------------------'
-      print 'latitude    ' , gpsd.fix.latitude
-      print 'longitude   ' , gpsd.fix.longitude
-      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-      print 'altitude (m)' , gpsd.fix.altitude
-      print 'eps         ' , gpsd.fix.eps
-      print 'epx         ' , gpsd.fix.epx
-      print 'epv         ' , gpsd.fix.epv
-      print 'ept         ' , gpsd.fix.ept
-      print 'speed (m/s) ' , gpsd.fix.speed
-      print 'climb       ' , gpsd.fix.climb
-      print 'track       ' , gpsd.fix.track
-      print 'mode        ' , gpsd.fix.mode
-      print
-      print 'sats        ' , gpsd.satellites
- 
-      time.sleep(.5) #set to whatever
- 
-  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-    print "\nKilling Thread..."
-    gpsp.running = False
-    gpsp.join() # wait for the thread to finish what it's doing
-  print "Done.\nExiting."
+    gpsp = GpsPoller()
+    try: 
+        gpsp.start() 
+        while True:
+            os.system('clear')
+            report = gpsp.get_current_value()
+            if report.keys()[0] == 'epx':
+                print report 
+            time.sleep(.5)
+    except (KeyboardInterrupt, SystemExit):
+        print "\nKilling Thread.."
+        gpsp.running = False 
+        gpsp.join()
+    print "Done.\nExiting." 
+        
---
+++ RaspberryPiFiles/gps_poll.py
@@ -0,0 +1,23 @@
+import os 
+from gps import *
+from time import *
+import time 
+import threading 
+
+class GpsPoller(threading.Thread):
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.session = gps(mode=WATCH_ENABLE)
+        self.current_value = None 
+        
+        def run(self):
+            while gpsp.running:
+                self.current_value = self.session.next() 
+            except StopIteration:
+                pass 
+                
+                
+        def get_current_value(self):
+            return self.current_value 
+        
