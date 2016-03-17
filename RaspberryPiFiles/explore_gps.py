e1c025d14569ed9ca03df99cce47e5107243e233
timmyreilly <timreilly@live.com>
2016-03-17 12:07:53 -0700

    stream

--- RaspberryPiFiles/explore_gps.py
+++ RaspberryPiFiles/explore_gps.py
@@ -5,7 +5,7 @@ class GpsPoller(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
-        self.session = gps(mode=WATCH_ENABLE)
+        self.session = stream(WATCH_ENABLE|WATCH_NEWSTYLE)
         self.current_value = None
         
     def get_current_value(self):
