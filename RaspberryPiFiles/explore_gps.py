a63a0e4bdf1f3ba67ebc215f3f1b6a50888d7e54
timmyreilly <timreilly@live.com>
2016-03-17 13:51:58 -0700

    session threading

--- RaspberryPiFiles/explore_gps.py
+++ RaspberryPiFiles/explore_gps.py
@@ -5,26 +5,35 @@ from gps import *
 class GpsPoller(threading.Thread):
 
     def __init__(self):
-        
         threading.Thread.__init__(self)
         self.session = gps() 
         self.session.stream(WATCH_ENABLE|WATCH_NEWSTYLE)
         self.current_value = None
+        self.exitApp = False 
         
     def get_current_value(self):
         return self.current_value
         
+    def time_to_exit():
+        exitApp = True 
+        os._exit()
+        
     def run(self):
-        try:
-            while True:
-                self.current_value = self.session.next()
-        except StopIteration:
-            pass 
+        while not self.exitApp: 
+            try:
+                while True:
+                    self.current_value = self.session.next()
+            except StopIteration:
+                pass 
+        os._exit() 
             
 if __name__ == '__main__':
     gpsp = GpsPoller()
     gpsp.start()
     
     while 1:
-        time.sleep(.5)
-        print gpsp.get_current_value() 
+        try:
+            time.sleep(.5)
+            print gpsp.get_current_value()  
+        except KeyboardInterrupt:
+            gpsdp.time_to_exit()
